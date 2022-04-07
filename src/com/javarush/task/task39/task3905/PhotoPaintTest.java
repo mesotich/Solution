41f950736900b23253f49e633d51ad30b9d0aaa9
Dmitry Shishkin <4144442@bk.ru>
2022-04-07 15:40:53 +0300

    Через рекурсию и синглтон

--- src/com/javarush/task/task39/task3905/PhotoPaintTest.java
+++ src/com/javarush/task/task39/task3905/PhotoPaintTest.java
@@ -1,33 +1,30 @@
 package com.javarush.task.task39.task3905;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PhotoPaintTest {
 
-    Color[][] image = {
-            {Color.YELLOW, Color.YELLOW, Color.VIOLET, Color.VIOLET},
-            {Color.YELLOW, Color.YELLOW, Color.VIOLET, Color.VIOLET},
-            {Color.VIOLET, Color.VIOLET, Color.YELLOW, Color.YELLOW},
-            {Color.VIOLET, Color.VIOLET, Color.YELLOW, Color.YELLOW},
-    };
-
     @Test
     public void paintFill() {
+        Color[][] image = {
+                {Color.YELLOW, Color.YELLOW, Color.VIOLET, Color.VIOLET},
+                {Color.YELLOW, Color.YELLOW, Color.VIOLET, Color.VIOLET},
+                {Color.VIOLET, Color.VIOLET, Color.YELLOW, Color.YELLOW},
+                {Color.VIOLET, Color.VIOLET, Color.YELLOW, Color.YELLOW},
+        };
         PhotoPaint pp = new PhotoPaint();
         pp.paintFill(image, 0, 0, Color.INDIGO);
-        Color[][] actual = pp.getImage();
         Color[][] expected = {
                 {Color.INDIGO, Color.INDIGO, Color.VIOLET, Color.VIOLET},
                 {Color.INDIGO, Color.INDIGO, Color.VIOLET, Color.VIOLET},
                 {Color.VIOLET, Color.VIOLET, Color.YELLOW, Color.YELLOW},
                 {Color.VIOLET, Color.VIOLET, Color.YELLOW, Color.YELLOW},
         };
-        Assert.assertEquals(actual.length, expected.length);
-        Assert.assertEquals(actual[0].length, expected[0].length);
-        for (int i = 0; i < actual.length; i++) {
-            for (int j = 0; j < actual[0].length; j++) {
-                Assert.assertEquals(expected[i][j], actual[i][j]);
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[0].length; j++) {
+                assertEquals(expected[i][j], image[i][j]);
             }
         }
     }
--- src/com/javarush/task/task39/task3905/Solution.java
+++ src/com/javarush/task/task39/task3905/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task39.task3905;
 
+import java.util.Arrays;
+
 public class Solution {
 
     public static void main(String[] args) {
+
 
     }
 }
